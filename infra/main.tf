e967beea6a73d034a4aad1a397cf16678bc5ac38
Mahesh Diwan <diwanmahesh11@gmail.com>
2026-01-29 22:15:17 +0530

    Final clean deployment: Source code only

--- infra/main.tf
+++ infra/main.tf
@@ -21,6 +21,13 @@ resource "aws_security_group" "linkedin_sg" {
     cidr_blocks = ["0.0.0.0/0"] # For SSH access
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
