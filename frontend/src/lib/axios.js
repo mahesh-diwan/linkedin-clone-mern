6ee84763bc1eb12ccadf07951137d7689d38289f
Mahesh Diwan <diwanmahesh11@gmail.com>
2026-01-30 00:03:28 +0530

    Final clean deployment: Source code only

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_BACKEND_URL + "/api/v1",
-	withCredentials: true,
+  // Change this to a relative path
+  baseURL: "/api/v1",
+  withCredentials: true,
 });
-
