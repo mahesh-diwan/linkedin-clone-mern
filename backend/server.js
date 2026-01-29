6523b2d49502c405f91b77ac4b6c4496949b2dc8
Mahesh Diwan <diwanmahesh11@gmail.com>
2026-01-30 00:34:31 +0530

    Final clean deployment: Source code only

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
 );
 app.use(express.json({ limit: "5mb" })); // parse JSON bodies
 app.use(cookieParser());
-// API Routes
+// API Route
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
